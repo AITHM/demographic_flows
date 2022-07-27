82af1570949cd787cb0eff8b65660c0fa3694108
Emma McBryde <emma.mcbryde@jcu.edu.au>
2022-07-27 15:20:43 +1000

    Update test_demographic_flow.m
    add in more random states to start, include zeros. to and from zero plus positive to zero to positive to test the system.
    assert three things to test the system
    change the axis to allow for larger values in the plots

--- demographic_flows/test_demographic_flow.m
+++ demographic_flows/test_demographic_flow.m
@@ -1,12 +1,11 @@
 clear
 % generate some tricky random state data, with zeros
-state1=[rand(5, 1)/8];
+state1 = rand(5, 1);
 state1(5) = 0;
-state2=state1/6;
-state2(2:5)=state2( 1)*[1,1,1,3];
-state3 = state2;
-state3( 1)= 0;
-state3( 5)= state3(5)*2;
+state2 = rand(5, 1);
+state2(1) = 0;
+state3 = rand(5, 1);
+
 
 % generate the time vector 
 tvec=[1, 10, 100];
@@ -28,11 +27,15 @@ demographic_flows = demography_rates_proportional6(tvec, all_states);
 [time_state, y_state] = return_epi(tvec, demographic_flows, state1);
 
 ages_and_vacc_at_start = y_state(:, 1);
+
 compare_model_and_y_at_start = [ages_and_vacc_at_start, state1]
 ages_and_vacc_at_10 = y_state(:, 10);
 compare_model_and_y_at_10 = [ages_and_vacc_at_10, state2]
 ages_and_vacc_at_100 = y_state(:, 100);
 compare_model_and_y_at_100 = [ages_and_vacc_at_100, state3]
+assert(sum(abs(diff(compare_model_and_y_at_start')))<.01, "modelled first time point does not align with data")
+assert(sum(abs(diff(compare_model_and_y_at_10')))<.01, "modelled second time point does not align with data")
+assert(sum(abs(diff(compare_model_and_y_at_100')))<.01, "modelled third time point does not align with data")
 %%
 % test that output is as expected
 % Create a stacked bar chart using the bar function
@@ -40,7 +43,7 @@ compare_model_and_y_at_100 = [ages_and_vacc_at_100, state3]
 figure(1)
 bar(y_state', 1, 'stack')
 % Adjust the axis limits
-axis([0 100 0 1])
+%axis([0 100 0 1])
 set(gca, 'XTick', 1:99)
 % Add title and axis labels
 title('states by day')
@@ -51,7 +54,7 @@ ylabel('vaccinated')
 figure(2)
 bar(compare_model_and_y_at_start', .5, 'stack')
 % Adjust the axis limits
-axis([.5 2.5 0 1])
+%axis([.5 2.5 0 1])
 set(gca, 'XTick', 1:6)
 % Add title and axis labels
 title('states by day')
@@ -63,7 +66,7 @@ ylabel('proportion by age group')
 figure(3)
 bar(compare_model_and_y_at_10', .5, 'stack')
 % Adjust the axis limits
-axis([.5 2.5 0 1])
+%axis([.5 2.5 0 1])
 set(gca, 'XTick', 1:6)
 % Add title and axis labels
 title('states at 10')
@@ -74,7 +77,7 @@ ylabel('proportion by age group')
 figure(4)
 bar(compare_model_and_y_at_100', .5, 'stack')
 % Adjust the axis limits
-axis([.5 2.5 0 1])
+%axis([.5 2.5 0 1])
 set(gca, 'XTick', 1:6)
 % Add title and axis labels
 title('states at 100')
